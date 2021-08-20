a89a365d379293505e0bbc2f0a5c936cb754aef9
Louis Marais <louis-marais@hotmail.fr>
2021-08-20 11:15:29 +0200

    simplification

--- exemple.js
+++ exemple.js
@@ -1,7 +1,5 @@
-let city = "Paris";
-
 function tempChange(city){ //On envoie une requete ajax a l'api pour obtenir la temp de city et on l'affiche
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
     let requete = new XMLHttpRequest();
     requete.open('GET', url);
     requete.responseType = 'json';
@@ -22,9 +20,9 @@ function tempChange(city){ //On envoie une requete ajax a l'api pour obtenir la 
     }
 }
 
-tempChange(city);
+tempChange("Paris");
 const button = document.querySelector('button') 
 button.addEventListener('click', ()=>{
-    city = prompt('De quelle ville souhaitez vous avoir la météo ?');
+    let city = prompt('De quelle ville souhaitez vous avoir la météo ?');
     tempChange(city);
 });// on mets un écouteur sur le click sur button qui lancera tempChange avec city en parametre
