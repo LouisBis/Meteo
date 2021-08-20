aad5ce35cffea06d6de3724c99673b5341c97fe1
Louis Marais <louis-marais@hotmail.fr>
2021-08-20 11:21:37 +0200

    passage des let en const

--- exemple.js
+++ exemple.js
@@ -1,14 +1,14 @@
 function tempChange(city){ //On envoie une requete ajax a l'api pour obtenir la temp de city et on l'affiche
     const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
-    let requete = new XMLHttpRequest();
+    const requete = new XMLHttpRequest();
     requete.open('GET', url);
     requete.responseType = 'json';
     requete.send();
     requete.onload = function (){
         if (requete.readyState === XMLHttpRequest.DONE){
             if (requete.status === 200){
-                let reponse = requete.response;
-                let temperature = Math.round(reponse.main.temp*10)/10;
+                const reponse = requete.response;
+                const temperature = Math.round(reponse.main.temp*10)/10;
                 console.log(temperature);
                 document.querySelector('#temp').textContent=temperature;
                 document.querySelector('h2').textContent=city;
