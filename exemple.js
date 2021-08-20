d024fe44d8d7a9cb76cbd024fc6280af6e28a478
Louis Marais <louis-marais@hotmail.fr>
2021-08-20 10:35:57 +0200

    simplification et correction de la déclaration des variables

--- exemple.js
+++ exemple.js
@@ -1,8 +1,7 @@
-let city = "Tokyo";
-let url  = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
+let city = "Paris";
 
-function tempChange(){ //On envoie une requete ajax a l'api pour obtenir la temp de city et on l'affiche
-    url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
+function tempChange(city){ //On envoie une requete ajax a l'api pour obtenir la temp de city et on l'affiche
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
     let requete = new XMLHttpRequest();
     requete.open('GET', url);
     requete.responseType = 'json';
@@ -17,15 +16,15 @@ function tempChange(){ //On envoie une requete ajax a l'api pour obtenir la temp
                 document.querySelector('h2').textContent=city;
             }
         }
+        else {
+            alert('Un problème est intervenu, merci de revenir plus tard.');
+        }
     }
 }
 
-
-function cityChange (){ //On affiche un prompte demandant city et on appel la requete ajax
+tempChange(city);
+const button = document.querySelector('button') 
+button.addEventListener('click', ()=>{
     city = prompt('De quelle ville souhaitez vous avoir la météo ?');
-    tempChange();
-}
-
-tempChange();
-let button = document.querySelector('button') 
-button.addEventListener('click', cityChange);// on mets un écouteur sur le click sur button qui lancera cityChange
+    tempChange(city);
+});// on mets un écouteur sur le click sur button qui lancera tempChange avec city en parametre
