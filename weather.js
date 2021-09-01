497e258f056e5cd6610767e6f9f9ee3b12b8eb0e
Louis Marais <louis-marais@hotmail.fr>
2021-09-01 15:25:19 +0200

    UPDATE, using jquery for a clearer code

--- weather.js
+++ weather.js
@@ -1,41 +1,35 @@
 function tempChange(city){ //On envoie une requete ajax a l'api pour obtenir la temp de city et on l'affiche
     const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
-    const requete = new XMLHttpRequest();
-    requete.open('GET', url);
-    requete.responseType = 'json';
-    requete.send();
-    requete.onload = function (){
-        if (requete.readyState === XMLHttpRequest.DONE){
-            if (requete.status === 200){
-                const reponse = requete.response;
-                const temperature = Math.round(reponse.main.temp*10)/10;
-                document.querySelector('#temp').textContent=temperature;
-                document.querySelector('h2').textContent=city;
-            }
-        }
-        else {
-            alert('Un problème est intervenu, merci de revenir plus tard.');
-        }
-    }
+    $.ajax({
+        url : url,
+        type : 'GET',
+        dataType : 'json',
+        success : (data)=> {
+            $('#temp').text(Math.round(data.main.temp*10)/10);
+            $('h2').text(city);
+        },
+        error : ()=> alert('Un problème est intervenu, merci de revenir plus tard.')
+    })
 }
 
 function darkTheme(){
-    "use strict";
-    const c = document.querySelectorAll("h1, h2, p, button#cityChange, button#darkMode");
-    if (document.querySelector('h1').classList.contains('dark')){
+    "use strict"
+    if ($('h1').hasClass('dark')){
         //si la balise h1 possède une classe dark c'est que le theme sombre est activé
         //alors on modifie nos classe pour passer en theme clair
-        document.body.style.backgroundColor = 'white';
-        for (let i = 0; i < 4;i++) c[i].className = 'light';
-        document.querySelector('button#darkMode').textContent = '☽';
+        $('body').css('backgroundColor','white');
+        $('h1, h2, p, button#cityChange, button#darkMode').removeClass('dark');
+        $('h1, h2, p, button#cityChange, button#darkMode').addClass('light');
+        $('button#darkMode').text('☽');
         localStorage.setItem('theme', 'light');
         //et on enregistre ce choix dans nos localStorage
     }
     else{
         //sinon on passe en thème sombre 
-        document.body.style.backgroundColor = '#112F41';
-        for (let i = 0; i < 4;i++) c[i].className = 'dark';
-        document.querySelector('button#darkMode').textContent = '☀';
+        $('body').css('backgroundColor','#112F41');
+        $('h1, h2, p, button#cityChange, button#darkMode').removeClass('light');
+        $('h1, h2, p, button#cityChange, button#darkMode').addClass('dark');
+        $('button#darkMode').text('☀');
         localStorage.setItem('theme', 'sombre');
         //et on enregistre ce choix dans nos localStorage
 
@@ -49,25 +43,17 @@ if ('geolocation' in navigator){
         enableHighAccuracy : true
     }
     navigator.geolocation.watchPosition((position)=>{ //on utilise watchPosition et non get pour suivre le mouvement
-        const url = 'https://api.openweathermap.org/data/2.5/weather?lon='+position.coords.longitude+'&lat='+position.coords.latitude+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
-        const requete = new XMLHttpRequest();
-        requete.open('GET', url);
-        requete.responseType = 'json';
-        requete.send();
-        requete.onload = function (){
-            if (requete.readyState === XMLHttpRequest.DONE){
-                if (requete.status === 200){
-                    const reponse = requete.response;
-                    const temperature = Math.round(reponse.main.temp*10)/10;
-                    document.querySelector('#temp').textContent=temperature;
-                    document.querySelector('h2').textContent= reponse.name;
-                }
-            }
-            else {
-                alert('Un problème est intervenu, merci de revenir plus tard.');
-            }
-        }
-
+        const urlGeoloc = 'https://api.openweathermap.org/data/2.5/weather?lon='+position.coords.longitude+'&lat='+position.coords.latitude+'&units=metric&appid=c7c8b5706d5c9090ddaf2b10dea45605';
+        $.ajax({
+            url : urlGeoloc,
+            type : 'GET',
+            dataType : 'json',
+            success : (data)=> {
+                $('#temp').text(Math.round(data.main.temp*10)/10);
+                $('h2').text(data.name);
+            },
+            error : ()=> alert('Un problème est intervenu, merci de revenir plus tard.')
+        })
     },tempChange('Paris'), option)
     //Si la géolocalisation est refusé alors on affiche paris 
 
@@ -77,12 +63,10 @@ else {
 }
 
 //tempChange("Paris"); // on défini un ville au chargement de la page
-const cityButton = document.querySelector('#cityChange');
-cityButton.addEventListener('click', ()=> tempChange(prompt('De quelle ville souhaitez vous avoir la météo ?')));
+$('#cityChange').click(()=> tempChange(prompt('De quelle ville souhaitez vous avoir la météo ?')));
 // on mets un écouteur sur le click de cityButton qui lancera tempChange avec un prompt en parametre
 
-const darkButton = document.querySelector('#darkMode');
-darkButton.addEventListener('click', darkTheme);
+$('#darkMode').click(darkTheme);
 // on mets un écouteur sur le click de darkButton qui lancera darkTheme, qui selon le mode actuelle s'adaptera
 
 if (localStorage.getItem('theme') == 'sombre'){
